fb4e9f070c5cd86664b4a740c2cc2df74c8be08d
mcbob98 <mcdevitt@ksu.edu>
2016-10-09 17:17:02 -0500

    Problem set 5

--- Hello.scala
+++ Hello.scala
@@ -1,8 +1,27 @@
-/**
-  * Created by mcdevitt on 9/23/16.
-  */
+
+
+import java.io.FileReader
+
+import scala.io.Source
+import java.io.{FileReader, FileNotFoundException, IOException}
+
 object Hello {
   def main(args: Array[String]): Unit = {
+
+    var dna = ""
+
+    val filename = "/net/files.cis.ksu.edu/exports/home/m/mcdevitt/Bigdata/ScalaTest/src/main/scala/dna.txt"
+    try {
+     for (line <- Source.fromFile(filename).getLines()) {
+       dna =(line.toString)
+       println(line)
+       println("test: " + dna)
+     }
+  } catch {
+          case ex: FileNotFoundException => println("Couldn't find that file.")
+          case ex: IOException => println("Had an IOException trying to read that file")
+    }
+
 
     def mod3(l:List[Int]) : List[Int] = {
       val rem:List[Int] = for(i <- l) yield i % 5
@@ -19,8 +38,21 @@ object Hello {
 
     println(mod3(nums))
     var test = (x:List[Int]) => for(x <- nums) yield x % 5
-    println("Test is: " + test(nums))
-    println("Vec: " + makeAVector(1))
-  }
+    print( test(nums).map(x => makeAVector(x)))
+    //println("Test is: " + test(nums))
+    // println("Vec: " + makeAVector(1))
+    println("dna: " + dna)
+
+    def kmer(d:String,k:Int): List[String] = {
+      var kmerList: List[String] = List()
+
+        kmerList = d.subSequence(0,d.length + k-1)
+      }
+
+     kmer(dns,5).map(s => 1).sorted(_ < _)
+    }
+
+    println(find_kmers(dna,5,dna.length+1))
+
 
 }
